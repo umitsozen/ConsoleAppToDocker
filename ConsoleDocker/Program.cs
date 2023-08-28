20eb2b1dab39c716b3000212967150fc95e2c7e8
umitsozen <uguneri@gmail.com>
2023-08-28 21:58:43 +0300

    Add project files.

---
+++ ConsoleDocker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleDocker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 1000)
+            {
+                Console.WriteLine("Hello world!" + i);
+                i++;
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
